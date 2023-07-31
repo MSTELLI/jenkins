b94bb8bb46e45ffe3345f25206239e3de38c9786
Süleyman TELLİOĞLU <44327597+MSTELLI@users.noreply.github.com>
2023-07-31 13:46:05 +0300

    Update airflow_first_file.py

--- dag/airflow_first_file.py
+++ dag/airflow_first_file.py
@@ -1,1 +1,19 @@
-print("sa from github")
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def helloWorld():
+    print("sa from github")
+
+with DAG(dag_id="hello_world_dag",
+         start_date=datetime(2021,1,1),
+         schedule_interval="@hourly",
+         catchup=False) as dag:
+
+
+
+		task1 = PythonOperator(
+		        task_id="hello_world",
+		        python_callable=helloWorld)
+
+task1
