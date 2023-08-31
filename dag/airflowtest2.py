f0d877d315ceb7a4878bc9a5003b59f1815ce20b
Suleyman Tellioglu <suleyman.tellioglu@hopi.com.tr>
2023-08-31 15:33:18 +0300

    changed something.

--- dag/airflowtest2.py
+++ dag/airflowtest2.py
@@ -2,18 +2,16 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
+
 def helloWorld():
     print("sa from github")
 
 with DAG(dag_id="hello from airflowtest2 new commit auto",
-         start_date=datetime(2021,1,1),
+         start_date=datetime(2021, 1, 1),
          schedule_interval="@hourly",
          catchup=False) as dag:
+    task2 = PythonOperator(
+        task_id="hello_world",
+        python_callable=helloWorld)
 
-
-
-		task1 = PythonOperator(
-		        task_id="hello_world",
-		        python_callable=helloWorld)
-
-task1
+task2
